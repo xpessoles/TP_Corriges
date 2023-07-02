cd37fcbf603e2782d84b64807c8ce091344e3df8
Xavier Pessoles <xpessoles.ptsi@free.fr>
2023-07-02 23:55:14 +0200

    MAJ

--- 04_Comax/Modeles_Matlab/parametres.m
+++ 04_Comax/Modeles_Matlab/parametres.m
@@ -1,13 +1,25 @@
 % Paramètres du moteur
-R = 0.299      ; % Ohm
-L = 0.0824e-3  ; % Henry
+Rm = 0.299      ; % Ohm
+Lm = 0.0824e-3  ; % Henry
 Kc = 30.2e-3   ; % Nm/A
 
 Ke = 317               ; %tr/min  /V
 Ke = 317*2*pi/60       ; %rad/s/V
+Kem = 1/317            ; %V/(tr/min) 
 J = 142                ; % g.cm²
 J = 142/1000/100/100   ; % kg.m²
 f = 0                  ;
+
+% Correcteur vitesse
+KP_EPOS = 2420;
+KI_EPOS = 8340;
+KD_EPOS = 3230;
+
+% Correcteur courant
+Kii_EPOS = 200;
+Kip_EPOS = 3150;
+
+
 
 % Paramètres mécaniques
 kr = 1/15.88        ; % Réducteur
@@ -31,11 +43,11 @@ vmax = vmax*2*pi/60; % rad/s
 L = 300             ; % mm distance à parcourir
 am = 300/1000 /rkr       ; % angle moteur an radian correspondant
 % Temps d'accélération : 
-t1 = vmax / amax
+t1 = vmax / amax        ;  
 % angle a vitesse constante : 
-avc = am - t1*vmax
+avc = am - t1*vmax      ;
 % temps à vitesse constante
-tvc = avc/vmax
+tvc = avc/vmax          ;
 
-t2 = t1+tvc
-t3 = tvc+2*t1
+t2 = t1+tvc             ;
+t3 = tvc+2*t1           ;
